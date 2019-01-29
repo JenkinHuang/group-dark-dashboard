536949e9ac43b8c061880d7cdd32a5cc612b3ade
Hikari <hikarizc@gmail.com>
2019-01-29 15:37:58 +0800

    Add User Profile Modal.

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
         type: "GET",
         url: "http://localhost:8080/v1/detail/count/user",
         beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODcyMTQ4NywidXNlcm5hbWUiOiJoaWthcmkifQ.N8EUPPB0NFMF3ZHYKpXShu7PDydGhuIpve-3ayg9LBk");
+            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODgwODI1MiwidXNlcm5hbWUiOiJoaWthcmkifQ.emja70Ou5kJys3plDYwz64qKzH6ECMMR8cJ8__lwiTQ");
         },
         success: function (result) {
             $("#user_count").append(result.data);
@@ -14,7 +14,7 @@ $(document).ready(function () {
         type: "GET",
         url: "http://localhost:8080/v1/detail/count/group",
         beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODcyMTQ4NywidXNlcm5hbWUiOiJoaWthcmkifQ.N8EUPPB0NFMF3ZHYKpXShu7PDydGhuIpve-3ayg9LBk");
+            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODgwODI1MiwidXNlcm5hbWUiOiJoaWthcmkifQ.emja70Ou5kJys3plDYwz64qKzH6ECMMR8cJ8__lwiTQ");
         },
         success: function (result) {
             $("#group_count").append(result.data);
@@ -25,7 +25,7 @@ $(document).ready(function () {
         type: "GET",
         url: "http://localhost:8080/v1/detail/count/topic",
         beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODcyMTQ4NywidXNlcm5hbWUiOiJoaWthcmkifQ.N8EUPPB0NFMF3ZHYKpXShu7PDydGhuIpve-3ayg9LBk");
+            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODgwODI1MiwidXNlcm5hbWUiOiJoaWthcmkifQ.emja70Ou5kJys3plDYwz64qKzH6ECMMR8cJ8__lwiTQ");
         },
         success: function (result) {
             $("#topic_count").append(result.data);
@@ -36,7 +36,7 @@ $(document).ready(function () {
         type: "GET",
         url: "http://localhost:8080/v1/detail/count/reply",
         beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODcyMTQ4NywidXNlcm5hbWUiOiJoaWthcmkifQ.N8EUPPB0NFMF3ZHYKpXShu7PDydGhuIpve-3ayg9LBk");
+            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODgwODI1MiwidXNlcm5hbWUiOiJoaWthcmkifQ.emja70Ou5kJys3plDYwz64qKzH6ECMMR8cJ8__lwiTQ");
         },
         success: function (result) {
             $("#reply_count").append(result.data);
--- js/tables.js
+++ js/tables.js
@@ -3,19 +3,31 @@ $(document).ready(function () {
         type: "GET",
         url: "http://localhost:8080/v1/user?page=1",
         beforeSend: function (xhr) {
-            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODcyMTQ4NywidXNlcm5hbWUiOiJoaWthcmkifQ.N8EUPPB0NFMF3ZHYKpXShu7PDydGhuIpve-3ayg9LBk");
+            xhr.setRequestHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiW2FkbWluXSIsImV4cCI6MTU0ODgwODI1MiwidXNlcm5hbWUiOiJoaWthcmkifQ.emja70Ou5kJys3plDYwz64qKzH6ECMMR8cJ8__lwiTQ");
         },
         success: function (result) {
             var list = result.data.list;
             for (var i = 0; i < list.length; i++) {
-                $("#tbody").append("<tr><td>" + list[i].username + "</td><td>" + list[i].email + "</td><td>" + list[i].gender + "</td>" + "<td class='td-actions text-center'><button type='button' rel='tooltip' class='btn btn-info btn-link btn-icon btn-sm'><i class='tim-icons icon-single-02'></i></button><button type='button' rel='tooltip' class='btn btn-success btn-link btn-icon btn-sm'><i class='tim-icons icon-settings'></i></button><button type='button' rel='tooltip' class='btn btn-danger btn-link btn-icon btn-sm'><i class='tim-icons icon-simple-remove'></i></button></td>" + "</tr>");
+                $("#tbody").append("<tr><td>" + list[i].username + "</td><td>" + list[i].email + "</td><td>" + list[i].gmtModified + "</td><td class='td-actions text-center'><button type='button' rel='tooltip' class='btn btn-info btn-link btn-icon btn-sm user-profile' username='" + list[i].username + "'><i class='tim-icons icon-single-02'></i></button><button type='button' rel='tooltip' class='btn btn-success btn-link btn-icon btn-sm user-setting' username='" + list[i].username + "'><i class='tim-icons icon-settings'></i></button><button type='button' rel='tooltip' class='btn btn-danger btn-link btn-icon btn-sm user-remove' username='" + list[i].username + "'><i class='tim-icons icon-simple-remove'></i></button></td></tr>");
             }
 
             var pages = result.data.pages;
 
-            for (var i = 2; i <= 15; i++) {
+            for (var i = 2; i <= pages; i++) {
                 $("#pagination").before("<li class='page-item'><a class='page-link' href='#'>" + i + "</a></li>");
             }
+
+            $(".user-profile").bind("click", function () {
+                $('#user-profile').modal();
+            });
+
+            $(".user-setting").bind("click", function () {
+                alert($(this).attr("username"));
+            });
+
+            $(".user-remove").bind("click", function () {
+                alert($(this).attr("username"));
+            });
         }
     });
 });
